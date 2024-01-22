f366db49da81154e5fb26e4da272c75ed0d35ef8
Shpytchuk-Vasyl <vasyl.shpytchuk.pz.2022@lpnu.ua>
2024-01-22 20:10:04 +0200

    added ability to complete plans

--- src/main/java/org/telegram/mybot/message/handlers/tracker/CompleteHandler.java
+++ src/main/java/org/telegram/mybot/message/handlers/tracker/CompleteHandler.java
@@ -3,8 +3,12 @@ package org.telegram.mybot.message.handlers.tracker;
 import org.telegram.mybot.ServiceManager;
 import org.telegram.mybot.message.Handler;
 import org.telegram.mybot.message.Sender;
+import org.telegram.mybot.tracker.entity.TrackerStatus;
 import org.telegram.mybot.user.entity.User;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 
 public class CompleteHandler extends Handler<CallbackQuery> {
     private final User user;
@@ -18,6 +22,14 @@ public class CompleteHandler extends Handler<CallbackQuery> {
 
     @Override
     public void resolve(CallbackQuery callbackQuery) {
-
+        serviceManager.getTrackerService().completeDailyPlan(Long.valueOf(callbackQuery.getData()));
+        SendMessage msg = TrackerHandler.getDailyPlans(user,
+                NavigationHandler.getDateFromText(callbackQuery.getMessage().getText()), serviceManager);
+        sender.sendEditMessage(EditMessageText.builder()
+                .text(msg.getText())
+                .chatId(msg.getChatId())
+                .messageId(callbackQuery.getMessage().getMessageId())
+                .replyMarkup((InlineKeyboardMarkup) msg.getReplyMarkup())
+                .build());
     }
 }
